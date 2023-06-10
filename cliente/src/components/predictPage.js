f7831c1830f8bfbf3ed91aa1f9bd9a6aa129abc2
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-10 14:17:35 -0500

    Versión 0.5

--- cliente/src/components/predictPage.js
+++ cliente/src/components/predictPage.js
@@ -1,10 +1,55 @@
 import HeaderComponent from "./headerComponent";
+import axios from "axios";
 
 export default function PredictPage() {
+
+    const predecirPrecio = async (e) => {
+        e.preventDefault();
+        try {
+            const url = 'https://realestate-backend-lq5v.onrender.com/api/predictPrice'
+            const testData = {
+                'Superficie ': [200],
+                'Número de habitaciones': [3],
+                'Número de baños': [2],
+                'Número de garajes': [1],
+                'Piso de ubicación': [5],
+                'Vista al exterior': [1],
+                'Años de antigüedad': [10],
+                'ate vitarte': [False],
+                'barranco': [True],
+                'bellavista': [False],
+                'breña': [False],
+                'carabayllo': [False],
+                'cercado de lima': [False],
+                'chorrillos': [False],
+                'comas': [False],
+                'jesús maría': [False],
+                'la molina': [False],
+                'la perla': [False],
+                'la victoria': [False],
+                'lince': [False],
+                'los olivos': [False],
+                'magdalena': [False],
+                'miraflores': [False],
+                'pueblo libre': [False],
+                'san borja': [False],
+                'san isidro': [False],
+                'san miguel': [False],
+                'surco': [False],
+                'surquillo': [False]
+            }
+            const response = await axios.post(url, testData)
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
             <HeaderComponent/>
             <h1>Cotizar</h1>
+            <button onClick={predecirPrecio}>Predecir</button>
         </div>
     )
 }
--- servidor/main.py
+++ servidor/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pandas as pd
 import dataManagement, learningModel
 
 app = Flask(__name__)
@@ -7,43 +8,49 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 datosProcesados = dataManagement.inicializarData()
 modelo = learningModel.generarModelo(datosProcesados)
-data = {
-    'Superficie ': [200],
-    'Número de habitaciones': [3],
-    'Número de baños': [2],
-    'Número de garajes': [1],
-    'Piso de ubicación': [5],
-    'Vista al exterior': [1],
-    'Años de antigüedad': [10],
-    'ate vitarte': [False],
-    'barranco': [True],
-    'bellavista': [False],
-    'breña': [False],
-    'carabayllo': [False],
-    'cercado de lima': [False],
-    'chorrillos': [False],
-    'comas': [False],
-    'jesús maría': [False],
-    'la molina': [False],
-    'la perla': [False],
-    'la victoria': [False],
-    'lince': [False],
-    'los olivos': [False],
-    'magdalena': [False],
-    'miraflores': [False],
-    'pueblo libre': [False],
-    'san borja': [False],
-    'san isidro': [False],
-    'san miguel': [False],
-    'surco': [False],
-    'surquillo': [False]
-}
-print("Valor predecido: ", learningModel.predecirPrecio(data, modelo))
 
 
-@app.get("/api")
-def api():
-    return jsonify('Hola')
+@app.post("/api/predictPrice")
+def predictPrice():
+    requestData = request.json
+
+    data = {
+        'Superficie': [requestData.get('Superficie')],
+        'Número de habitaciones': [requestData.get('Número de habitaciones')],
+        'Número de baños': [requestData.get('Número de baños')],
+        'Número de garajes': [requestData.get('Número de garajes')],
+        'Piso de ubicación': [requestData.get('Piso de ubicación')],
+        'Vista al exterior': [requestData.get('Vista al exterior')],
+        'Años de antigüedad': [requestData.get('Años de antigüedad')],
+        'ate vitarte': [requestData.get('ate vitarte')],
+        'barranco': [requestData.get('barranco')],
+        'bellavista': [requestData.get('bellavista')],
+        'breña': [requestData.get('breña')],
+        'carabayllo': [requestData.get('carabayllo')],
+        'cercado de lima': [requestData.get('cercado de lima')],
+        'chorrillos': [requestData.get('chorrillos')],
+        'comas': [requestData.get('comas')],
+        'jesús maría': [requestData.get('jesús maría')],
+        'la molina': [requestData.get('la molina')],
+        'la perla': [requestData.get('la perla')],
+        'la victoria': [requestData.get('la victoria')],
+        'lince': [requestData.get('lince')],
+        'los olivos': [requestData.get('los olivos')],
+        'magdalena': [requestData.get('magdalena')],
+        'miraflores': [requestData.get('miraflores')],
+        'pueblo libre': [requestData.get('pueblo libre')],
+        'san borja': [requestData.get('san borja')],
+        'san isidro': [requestData.get('san isidro')],
+        'san miguel': [requestData.get('san miguel')],
+        'surco': [requestData.get('surco')],
+        'surquillo': [requestData.get('surquillo')]
+    }
+
+    table = pd.DataFrame(data)
+
+    prediccion = modelo.predict(table)
+
+    return jsonify(prediccion.tolist())
 
 
 if __name__ == "__main__":
