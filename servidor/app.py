c316baa4d98939e6f8bed617cd9f883889c7962e
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-09 19:35:14 -0500

    Versión 0.2

--- servidor/app.py
+++ servidor/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://zesty-alpaca-99e0b5.netlify.app"}})
+CORS(app, resources={r"/api/*": {"origins": "https://zesty-alpaca-99e0b5.netlify.app/"}})
 
 
 @app.route('/api', methods=['POST'])
