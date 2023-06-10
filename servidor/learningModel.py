b5ba30c1e0b79ca8c6b5bdcb75fde22ba968a8e6
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-10 13:17:00 -0500

    Versión 0.4

--- servidor/learningModel.py
+++ servidor/learningModel.py
@@ -1,22 +1,32 @@
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_absolute_error
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 
 def generarModelo(data):
     parametros = data.drop(['Precio en dólares corrientes'], axis=1)
     variableObjetivo = data['Precio en dólares corrientes']
+    X_train, X_test, y_train, y_test = train_test_split(parametros, variableObjetivo, test_size=0.2, random_state=42)
 
-    modelo = Ridge()
-    modelo.fit(parametros, variableObjetivo)
-    print("Intercepto del modelo: ", modelo.intercept_)
+    # Crear el modelo de Random Forest para regresión
+    random_forest = RandomForestRegressor(n_estimators=200, max_depth=15, min_samples_split=5)
 
-    prediccion = modelo.predict(parametros)
-    print('R^2 = ', modelo.score(parametros, variableObjetivo))
-    print('MAE = ', mean_absolute_error(variableObjetivo, prediccion))
+    # Entrenar el modelo
+    random_forest.fit(X_train, y_train)
 
-    tablaComparacion = parametros
-    tablaComparacion['Precio'] = variableObjetivo
-    tablaComparacion['PrecioPred'] = prediccion
-    tablaComparacion['Diferencia'] = (prediccion / variableObjetivo - 1) * 100
-    print(tablaComparacion)
+    # Realizar predicciones en el conjunto de prueba
+    predicciones = random_forest.predict(X_test)
 
+    # Calcular el coeficiente R2
+    r2 = r2_score(y_test, predicciones)
+
+    # Imprimir el coeficiente R2
+    print("Coeficiente R2: ", r2)
+    return random_forest
+
+
+def predecirPrecio(datos, modelo):
+    table = pd.DataFrame(datos)
+    prediccion = modelo.predict(table)
+    return prediccion
--- servidor/main.py
+++ servidor/main.py
@@ -7,6 +7,38 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 datosProcesados = dataManagement.inicializarData()
 modelo = learningModel.generarModelo(datosProcesados)
+data = {
+    'Superficie ': [200],
+    'Número de habitaciones': [3],
+    'Número de baños': [2],
+    'Número de garajes': [1],
+    'Piso de ubicación': [5],
+    'Vista al exterior': [1],
+    'Años de antigüedad': [10],
+    'ate vitarte': [False],
+    'barranco': [True],
+    'bellavista': [False],
+    'breña': [False],
+    'carabayllo': [False],
+    'cercado de lima': [False],
+    'chorrillos': [False],
+    'comas': [False],
+    'jesús maría': [False],
+    'la molina': [False],
+    'la perla': [False],
+    'la victoria': [False],
+    'lince': [False],
+    'los olivos': [False],
+    'magdalena': [False],
+    'miraflores': [False],
+    'pueblo libre': [False],
+    'san borja': [False],
+    'san isidro': [False],
+    'san miguel': [False],
+    'surco': [False],
+    'surquillo': [False]
+}
+print("Valor predecido: ", learningModel.predecirPrecio(data, modelo))
 
 
 @app.post("/api")
