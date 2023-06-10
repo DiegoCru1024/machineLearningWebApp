8608e5697a5f8ecb24d500fd48fbb196cd6ad0e8
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-10 15:04:51 -0500

    Versión 0.5

--- servidor/main.py
+++ servidor/main.py
@@ -14,43 +14,44 @@ modelo = learningModel.generarModelo(datosProcesados)
 def predictPrice():
     requestData = request.json
 
-    data = {
-        'Superficie': [requestData.get('Superficie')],
-        'Número de habitaciones': [requestData.get('Número de habitaciones')],
-        'Número de baños': [requestData.get('Número de baños')],
-        'Número de garajes': [requestData.get('Número de garajes')],
-        'Piso de ubicación': [requestData.get('Piso de ubicación')],
-        'Vista al exterior': [requestData.get('Vista al exterior')],
-        'Años de antigüedad': [requestData.get('Años de antigüedad')],
-        'ate vitarte': [requestData.get('ate vitarte')],
-        'barranco': [requestData.get('barranco')],
-        'bellavista': [requestData.get('bellavista')],
-        'breña': [requestData.get('breña')],
-        'carabayllo': [requestData.get('carabayllo')],
-        'cercado de lima': [requestData.get('cercado de lima')],
-        'chorrillos': [requestData.get('chorrillos')],
-        'comas': [requestData.get('comas')],
-        'jesús maría': [requestData.get('jesús maría')],
-        'la molina': [requestData.get('la molina')],
-        'la perla': [requestData.get('la perla')],
-        'la victoria': [requestData.get('la victoria')],
-        'lince': [requestData.get('lince')],
-        'los olivos': [requestData.get('los olivos')],
-        'magdalena': [requestData.get('magdalena')],
-        'miraflores': [requestData.get('miraflores')],
-        'pueblo libre': [requestData.get('pueblo libre')],
-        'san borja': [requestData.get('san borja')],
-        'san isidro': [requestData.get('san isidro')],
-        'san miguel': [requestData.get('san miguel')],
-        'surco': [requestData.get('surco')],
-        'surquillo': [requestData.get('surquillo')]
-    }
+    data = [{
+        'Superficie ': requestData.get('Superficie'),
+        'Número de habitaciones': requestData.get('Número de habitaciones'),
+        'Número de baños': requestData.get('Número de baños'),
+        'Número de garajes': requestData.get('Número de garajes'),
+        'Piso de ubicación': requestData.get('Piso de ubicación'),
+        'Vista al exterior': requestData.get('Vista al exterior'),
+        'Años de antigüedad': requestData.get('Años de antigüedad'),
+        'ate vitarte': requestData.get('ate vitarte'),
+        'barranco': requestData.get('barranco'),
+        'bellavista': requestData.get('bellavista'),
+        'breña': requestData.get('breña'),
+        'carabayllo': requestData.get('carabayllo'),
+        'cercado de lima': requestData.get('cercado de lima'),
+        'chorrillos': requestData.get('chorrillos'),
+        'comas': requestData.get('comas'),
+        'jesús maría': requestData.get('jesús maría'),
+        'la molina': requestData.get('la molina'),
+        'la perla': requestData.get('la perla'),
+        'la victoria': requestData.get('la victoria'),
+        'lince': requestData.get('lince'),
+        'los olivos': requestData.get('los olivos'),
+        'magdalena': requestData.get('magdalena'),
+        'miraflores': requestData.get('miraflores'),
+        'pueblo libre': requestData.get('pueblo libre'),
+        'san borja': requestData.get('san borja'),
+        'san isidro': requestData.get('san isidro'),
+        'san miguel': requestData.get('san miguel'),
+        'surco': requestData.get('surco'),
+        'surquillo': requestData.get('surquillo')
+    }]
 
     table = pd.DataFrame(data)
+    print(table)
 
     prediccion = modelo.predict(table)
 
-    return jsonify(prediccion)
+    return jsonify(prediccion.tolist())
 
 
 if __name__ == "__main__":
